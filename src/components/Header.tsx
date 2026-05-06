a4c1c6e6525d0faf0e25a5c67d8e517dba460185
fabianalvarezmo-stack <fabianalvarezmo@gmail.com>
2026-05-06 01:07:50 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
         {/* <Link to="/" className="flex items-center gap-3"> */}
           <div className="h-10 md:h-14 overflow-hidden flex items-center">
             <img 
-               src="../../img/logo.png" 
+               src="../../1.jpg"
                alt="Notebooks y Pantallas" 
                className="h-full w-auto object-contain brightness-0 invert" 
                referrerPolicy="no-referrer"
