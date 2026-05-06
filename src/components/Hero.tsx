4651cebaa3cb16f79879420561ba9ead6dbef772
fabianalvarezmo-stack <fabianalvarezmo@gmail.com>
2026-05-06 00:15:21 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from 'motion/react';
 import { ArrowRight, Monitor } from 'lucide-react';
 
 const carouselImages = [
-  "../../../img/Imagen1.jpg",
+  "../../img/Imagen1.jpg",
   "../../../img/P2curva.JPG",
   "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?auto=format&fit=crop&q=80&w=1200",
   "https://images.unsplash.com/photo-1603302576837-37561b2e2302?auto=format&fit=crop&q=80&w=1200",
